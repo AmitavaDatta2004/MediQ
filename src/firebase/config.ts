bf6a7c18e1fbdb83de8e0fdcd7a7b4abaa95d722
AmitavaPranay <legendaryduo202327@gmail.com>
2025-12-24 18:58:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3593689426-8cf5b",
+  "appId": "1:127346105286:web:63f5bf512f2e08d4fc30c7",
+  "apiKey": "AIzaSyC059fFBJx2dcC5O8p9AaOURRXqJtCuB-8",
+  "authDomain": "studio-3593689426-8cf5b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "127346105286"
+};
